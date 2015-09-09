10f03a18c28286e6e754069e26dca741f50b14f3
unknown <s.opanasenko@admin.pakscompany.local>
2015-09-09 18:32:11 +0300

    First

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,21 @@
 
-var gulp = require("gulp"),
-connect = require("gulpconnect")
-;
-// Запуск сервера
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+ 
 gulp.task('connect', function() {
-connect.server({
-root: 'app',
-livereload: true,
-port: 8888
-});
-});
-// Работа с html
+  connect.server({
+    root: '',
+    livereload: true
+  });
+});
+ 
 gulp.task('html', function () {
-gulp.src('./app/*.html')
-.pipe(connect.reload());
-});
-// Работа с css
-gulp.task('css', function () {
-gulp.src('./app/css/*.css')
-.pipe(connect.reload());
-});
-// Работа с js
-gulp.task('js', function () {
-gulp.src('./app/js/*.js')
-.pipe(connect.reload());
-});
-// Слежка
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+ 
 gulp.task('watch', function () {
-gulp.watch(['./app/*.html'], ['html']);
-gulp.watch(['./app/css/*.css'], ['css']);
-gulp.watch(['./app/js/*.js'], ['js']);
-});
-// Задача поумолчанию
-gulp.task('default', ['connect', 'watch']);
+  gulp.watch(['./*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
